a00dd5f1deb769cb8330ef8508f6b9852a010803
Watapax <watapax@gmail.com>
2023-04-26 22:07:26 -0400

    setting static

--- serapi/settings.py
+++ serapi/settings.py
@@ -136,6 +136,8 @@ USE_TZ = False
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
